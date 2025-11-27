77b074c6e2e11ab5947b131b3041a76e041c98a7
erick <7135739@continental.edu.pe>
2025-11-27 13:29:16 -0500

    main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ n = int(input("Ingresa el número de término que quieres (n): "))
 an = a1 + (n - 1) * d
 
 print("El término número", n, "de la progresión aritmética es:", an)
+
+si r != 1 
+      sn = a1*(r**n-1)/r-1
+      caso centrario
+      sn = a1*n
